e02e8bad3be1079a1ddb3596f1f7120968c3085b
Shohei Ihaya <ihaya.shohei@me.com>
2018-11-04 11:46:56 +0900

    Add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
+  entry: ['./frontend/index.jsx'],
+  optimization: {
+    minimizer: [new UglifyJsPlugin()]
+  },
+  output: {
+    path: path.join(__dirname, 'static/js'),
+    publicPath: '/static/js/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-react'],
+                ['@babel/preset-env', { useBuiltIns: 'usage' }]
+              ],
+              plugins: ['@babel/plugin-proposal-class-properties']
+            }
+          }
+        ]
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
+};
